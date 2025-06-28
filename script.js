6b34e2db568182a65e8df136c3b9d783160697de
Shadowgamesps5 <jonatan_luiz_jb@hotmail.com>
2025-06-28 15:56:17 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+
+async function carregarLikes() {
+    try {
+        const res = await fetch('data.json?_=' + new Date().getTime());
+        const data = await res.json();
+
+        const container = document.getElementById("like-container");
+        container.innerHTML = ""; // limpa antes
+
+        data.forEach(item => {
+            const div = document.createElement("div");
+            div.className = "like-box";
+            div.innerText = `${item.nome} +${item.likes} ❤️`;
+            container.appendChild(div);
+        });
+    } catch (error) {
+        console.error("Erro ao carregar JSON:", error);
+    }
+}
+
+// Atualiza a cada 4 segundos
+setInterval(carregarLikes, 4000);
